d2a1f2da826ce95d4fd99ca5b68ce28c42a2a6c5
Isaac Torres <isaacjhon.tm@gmail.com>
2025-05-04 21:48:43 -0500

    eliminando algunos archivos

--- Clase02_25-03-2025/Usuarios/src/Usuarios/Usuarios.Application/Abstractions/Data/ISqlConnectionFactory.cs
+++ Clase02_25-03-2025/Usuarios/src/Usuarios/Usuarios.Application/Abstractions/Data/ISqlConnectionFactory.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Usuarios.Application.Abstractions.Data
-{
+namespace Usuarios.Application.Abstractions.Data;
     public interface ISqlConnectionFactory
     {
-        
+        IDbConnection CreateConnection();
     }
-}
